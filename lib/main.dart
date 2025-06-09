9fde77cb26ce049b12f766bcdcb1cfa9c1a68376
Khaledsamy31 <ksamy3166@gmail.com>
2025-06-10 02:15:54 +0300

    create list view

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: "Poppins",
         brightness: Brightness.dark
       ),
       home: const NotesView(),
